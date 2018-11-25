6624b643184d4dbebe77361982d9ded863cc36c5
taoufik321 <taoufik321@users.noreply.github.com>
2018-11-25 15:43:42 +0100

    Opzet gemaakt van opdracht 3 optie 1.

---
+++ src/main/java/opdracht3/optie1/Main.java
@@ -0,0 +1,27 @@
+package opdracht3.optie1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // ArrayList maken van de mogelijke tekens
+
+        // ArrayList maken van de mogelijke nummers
+
+        // Scanner aangemaakt om de naam van de speler in te voegen
+        Scanner sc = new Scanner(System.in);
+        String naam = sc.nextLine();
+        System.out.println(naam);
+
+        // Ken een random kaart toe aan de aangemaakte speler
+
+        // Als de speler ervoor kiest om verder te gaan geef dan nog een kaart
+
+        // Als de speler ervoor kiest om te stoppen kijk dan of de waarde(s) van de kaart(en) niet hoger is dan 21
+
+        // Als dit wel zo is dan heeft de speler verloren
+
+        // Als dit niet zo is dan heeft de speler gewonnen
+
+    }
+}
